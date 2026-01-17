cbf3b89dc4b7919481a51c57f829324defc9902e
haeming <goalsgoals0417@gmail.com>
2026-01-17 15:28:25 +0900

    chore: 프론트엔드 초기 환경 세팅

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home(){
+    return(
+        <>
+            <div className="row">
+                <div className="col">
+                    안녕하세요.
+                </div>
+            </div>
+        </>
+    )
+}
