73a3db7a025d6aaaf5e0c81cf71928d2de30fd87
haeming <goalsgoals0417@gmail.com>
2026-01-19 21:35:00 +0900

    feat: 초기 홈 화면 구성 및 SEO 메타 태그 적용 (#1)
    * chore: Tailwind 단독 사용으로 전환 및 기본 레이아웃 구성
    
    * chore: .gitignore 정리 및 프론트엔드 환경 파일 제외
    
    * feat: home 상단 이미지와 머릿말 작성
    
    * feat: TopBar.jsx 구성 및 home 화면에 적용
    
    * feat: Topbar Home 버튼에 홈 라우트 연결
    
    * feat: SEO 메타 태그 및 noscript 크롤러 대응 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import "../css/Home.css"
+
 export default function Home(){
     return(
         <>
-            <div className="row">
-                <div className="col">
-                    안녕하세요.
+            <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+                <div className="md:col-span-3">
+                    <img
+                        src="/guest_tab_logo.png"
+                        className="
+                        w-full
+                        max-w-[240px]
+                        mx-auto
+                        md:max-w-none
+                        object-contain
+                    "
+                        alt="HaemCake"/>
+                </div>
+                <div className="md:col-span-9 home-chat-bg my-auto">
+                    <p>환영합니다!</p>
+                    <p><span className="home-text-blog-info">HaemCake blog</span> 입니다.</p>
+                    <p>
+                        실제
+                        <span className="home-text-bold"> 서비스에서 마주한 문제</span>
+                        들과
+                        <span className="home-text-bold"> 성장하는 기록</span>
+                        들을 남깁니다.
+                        <span className="home-text-bold">(=^o^=)</span>
+                    </p>
                 </div>
             </div>
         </>
