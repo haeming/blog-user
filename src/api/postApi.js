ab498464d85a2cd8316a28d49e29dae44b3ec327
haeming <goalsgoals0417@gmail.com>
2026-01-27 22:09:10 +0900

    refactor: 프론트엔드 도메인 기반 구조 개편 및 CSS 배치 최적화

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,13 +1,13 @@
-import {request} from "./request.js";
+import {axiosInstance} from "./axiosInstance.js";
 
 export default function postApi(){
     const getPost = async (id) => {
-        return await request("get", `/api/guest/posts/${id}`, null);
+        return await axiosInstance("get", `/api/guest/posts/${id}`, null);
     }
 
     const getPosts = async( page = 0, size = 10, sort="createdAt,desc" ) => {
         const params = new URLSearchParams({ page, size, sort }).toString();
-        return await request("get", `/api/guest/posts?${params}`, null);
+        return await axiosInstance("get", `/api/guest/posts?${params}`, null);
     }
 
     return {getPost, getPosts};
