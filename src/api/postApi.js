1b2670a596a33970f75f9bc1e2a5446c4f4a9523
haeming <goalsgoals0417@gmail.com>
2026-02-09 22:18:56 +0900

    fix: 메인 화면 게시글 목록 조회 API 경로 수정

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -4,14 +4,14 @@ export default function postApi(){
     const getPost = (id) => {
         return axiosInstance({
             method: "get",
-            url: `/api/guest/posts/${id}`,
+            url: `/api/posts/${id}`,
         });
     }
 
     const getPosts = (page = 0, size = 10, sort="createdAt,desc") => {
         return axiosInstance({
             method: "get",
-            url: "/api/guest/posts",
+            url: "/api/posts",
             params: { page, size, sort },
         });
     };
