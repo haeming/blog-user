3d67360323140d92f246bedda04132adb9d7f2ad
haeming <goalsgoals0417@gmail.com>
2026-02-09 21:10:51 +0900

    refactor: axiosInstance를 설정 객체 기반으로 리팩토링

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,12 +1,14 @@
 import baseURL from "../config/apiBaseUrl.js";
 import axios from "axios";
 
-export const axiosInstance = async(method, url, data) => {
+export const axiosInstance = async({method, url, data, params}) => {
     try{
         const response = await axios({
             method,
-            url: `${baseURL}${url}`,data, ...null
-        })
+            url: `${baseURL}${url}`,
+            data,
+            params,
+        });
         return response.data;
     } catch (error){
         console.error("API error", error);
