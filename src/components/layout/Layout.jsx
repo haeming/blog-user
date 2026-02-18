6a75efcc97779fcd12ad592b6aec8a60bb96193f
haeming <goalsgoals0417@gmail.com>
2026-02-18 22:20:28 +0900

    refactor: 상단 info 제거 후 사이드바로 위치 이동

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,26 @@
 import "./Layout.css"
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import TopBar from "./TopBar.jsx";
+import SideBar from "./SideBar.jsx";
 
 export default function Layout(){
     return (
         <div className="min-h-screen home-bg">
             <TopBar/>
-            <div className="w-full mx-auto p-3 md:px-4 md:max-w-6xl md:pt-20">
-                <Outlet/>
+
+            {/* 콘텐츠 영역 */}
+            <div className="w-full mx-auto md:max-w-6xl md:pt-20 flex">
+
+                {/* 좌측 사이드바 */}
+                <aside className="w-64 hidden md:block">
+                    <SideBar/>
+                </aside>
+
+                {/* 메인 콘텐츠 */}
+                <main className="flex-1 p-3 md:px-4">
+                    <Outlet/>
+                </main>
+
             </div>
         </div>
     );
